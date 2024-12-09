2133079576bcb73a29b7d6e6a59d46af14c84a06
Sohini Purkait <purkaitsohini20@gmail.com>
2024-12-09 22:00:02 +0530

    RE assignments

---
+++ Recursion Assignments/checkPalindromeRE.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+bool isPalindrome(string& s, int start, int end){
+    //base case
+    if(start >= end){
+        return true;
+    }
+
+    //1 case
+    if(s[start] != s[end]){
+        return false;
+    }
+
+    //recursion
+    return isPalindrome(s, start+1, end-1);
+
+
+}
+
+int main(){
+    string s;
+    cin >> s;
+    cout << isPalindrome(s, 0, s.size()-1) << endl;
+    return 0;
+}
