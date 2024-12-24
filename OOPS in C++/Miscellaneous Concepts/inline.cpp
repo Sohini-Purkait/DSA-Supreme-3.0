b3b49297ae0337d71324db1a1d7fdbcb6324c394
Sohini Purkait <purkaitsohini20@gmail.com>
2024-12-24 11:13:01 +0530

    friend virtual inline

---
+++ OOPS in C++/Miscellaneous Concepts/inline.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+inline void numberShow(int num){
+    cout << num << endl;
+}
+
+int main(){
+
+    numberShow(12);
+    numberShow(12);
+    numberShow(12);
+    numberShow(12);
+    numberShow(12);
+    numberShow(12);
+    numberShow(12);
+    numberShow(12);
+    numberShow(12);
+    numberShow(12);
+    numberShow(12);
+    numberShow(12);
+    
+    return 0;
+}
